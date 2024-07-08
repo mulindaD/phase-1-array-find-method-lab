a3b51d861e03c7c6ed2ab8195b0dc109a8d3563e
Dominic Mulinda <mulindarweshumba@gmail.com>
2024-07-08 12:54:12 +0300

    Complete the .find() lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
+
+function superbowlWin(arr){
+    if(arr.find(isWin) === undefined) {
+        return undefined
+    } else {
+        return arr.find(isWin).year    
+    }
+}
+
+function isWin (element) {
+    if (element.result === 'W'){
+        return element.year
+    }
+}
+
+superbowlWin(record)
+console.log(superbowlWin(record))
+
